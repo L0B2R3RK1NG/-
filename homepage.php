24ebcf106b5e9743ef203e1b746ac6eb91b5ff4e
Amaar <amaargroep2a@outlook.com>
2024-04-25 10:39:49 +0200

    commit message

---
+++ homepage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <section class="hero">
+        <div class="main-witdh">
+            <header>
+                <div class="logo">
+                    <h2><a href="#">アニメ金庫</a></h2>
+                </div>
+                <nav>
+                    <ul>
+                        <li class="active"><a href="#">seasons</a></li>
+                        <li><a href="#">seasons</a></li>
+                        <li><a href="#">seasons</a></li>
+                        <li class="btn"><a href="#">play now</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <div class="content">
+                <div class="main-text">
+                    <h5>kieka</h5>
+                    <h1>mikiea</h1>
+                    <p>...lorem ipsem...</p>
+                    <a href="#">wachtch nowwa</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+</body>
+
+</html>
